28009dfaf680f31a52dcfd6310b87aa32d29caa6
Rohit ji <negiji@9gmail.com>
2026-06-08 19:05:28 +0530

    hello

---
+++ Lecture85/428. Bellman-Ford.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // code here
+
+        // 
+        vector<int>dist(V,100000000);
+        dist[src] = 0;
+
+        // relax all the edges V-1 times
+
+        for(int i=0;i<V-1;i++){
+
+            for(auto edge: edges){
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+
+                if(dist[u]==100000000) 
+                continue;
+
+                if(dist[v]> dist[u]+wt){
+                    dist[v] = dist[u]+wt;
+                }
+            }
+
+        }
+
+
+
+        // 1 last time relax the edges, if dist valye change, answer is not possible
+
+        for(auto edge: edges){
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+
+
+                if(dist[u]==100000000) 
+                continue;
+                 
+
+                if(dist[v]> dist[u]+wt){
+                    vector<int>ans;
+                    ans.push_back(-1);
+                    return ans;
+                }
+        }
+
+
+        return dist;
+        
+    }
+};
